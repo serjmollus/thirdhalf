b233127ac2462476051bffaad78e73af04ddaff8
Ortiz Gil, Sergio <extern.ortiz_gil-sergio@allianz.de>
2018-10-27 23:29:26 +0200

    Initial commit

---
+++ src/main/java/com/serjava/thirdhalf/ThirdhalfApplication.java
@@ -0,0 +1,12 @@
+package com.serjava.thirdhalf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdhalfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThirdhalfApplication.class, args);
+	}
+}
---
+++ src/test/java/com/serjava/thirdhalf/ThirdhalfApplicationTests.java
@@ -0,0 +1,16 @@
+package com.serjava.thirdhalf;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ThirdhalfApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
